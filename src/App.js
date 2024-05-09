4338b60054e9040750e6347da5dbd5b61ecfbc07
arunkumar0398 <32316065+arunkumar0398@users.noreply.github.com>
2024-05-09 23:53:12 +0530

    add: update calculation for maxMinCrop
    - added mantine provider wrapper to the App
    - added mantine table for displaying the crops with max and min production on each year
    - added styling for the table maxMinCrop

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
 import './App.css';
+import { Table } from '@mantine/core';
+import { calculateMaxMinCrop } from './utils/calculateMaxMinCrop';
 
 function App() {
+
+  const maxMinCrops = calculateMaxMinCrop().map((cropData, index) => (
+    <Table.Tr key={cropData.Year}>
+      <Table.Td>{cropData.Year}</Table.Td>
+      <Table.Td>{cropData.CropWithMaxProduction}</Table.Td>
+      <Table.Td>{cropData.CropWithMinProduction}</Table.Td>
+    </Table.Tr>
+  ));
+
   return (
     <>
+      <Table striped highlightOnHover withTableBorder withColumnBorders id='Crop--maxProd--minProd'>
+        <Table.Thead>
+          <Table.Tr>
+            <Table.Th>Year</Table.Th>
+            <Table.Th>Crop with Maximum Production in that Year</Table.Th>
+            <Table.Th>Crop with Minimum Production in that Year</Table.Th>
+          </Table.Tr>
+        </Table.Thead>
+        <Table.Tbody>{maxMinCrops}</Table.Tbody>
+      </Table>
+
+      <Table>
+      </Table>
     </>
   );
 }
